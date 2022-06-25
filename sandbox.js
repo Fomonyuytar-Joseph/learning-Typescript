2598fad5cabe9b28d7c916265f3483e9f8692e95
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-06-25 13:16:13 +0100

    init

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,15 @@
 var character;
 var age;
 var isLoggedIn;
+//arrays
+var ninjas = [];
+ninjas.push('joe');
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(true);
+console.log(mixed);
+//union types
+var uid;
+uid = '123';
+uid = 123;
--- sandbox.ts
+++ sandbox.ts
@@ -3,4 +3,22 @@ let character: string;
 let age:number;
 let isLoggedIn: boolean;
 
+//arrays
+let ninjas: string[]=[];
+
+ninjas.push('joe')
+
+let mixed :(string | number | boolean )[]=[];
+
+mixed.push('hello')
+mixed.push(20)
+mixed.push(true)
+
+console.log(mixed)
+
+//union types
+
+let uid : string|number
+uid='123'
+uid=123
 
