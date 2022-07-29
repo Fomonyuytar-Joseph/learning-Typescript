289f2c5dc735ad59ae5cc4d610187a566b6d91ae
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-29 17:10:31 +0100

    just learnt type aliases

--- public/app.js
+++ public/app.js
@@ -1,117 +1,49 @@
-//TYPE ALIASES
-type StringorNum = string | number;
-type objWithName = {name:string ,uid:StringorNum}
-
-
-const logDetails = (uid: StringorNum , item: string)=>{
-    console.log(`${item} has a ui of ${uid}`)
-}
-
-const greet =(user:objWithName) =>{
-    console.log(`${user.name} says hi`);
-    
-}
-
-
+"use strict";
+// //TYPE ALIASES
+// type StringorNum = string | number;
+// type objWithName = {name:string ,uid:StringorNum}
+// const logDetails = (uid: StringorNum , item: string)=>{
+//     console.log(`${item} has a ui of ${uid}`)
+// }
+// const greet =(user:objWithName) =>{
+//     console.log(`${user.name} says hi`);
+// }
 //FUNCTIONS
 // let greet : Function
-
 // greet= ()=>{
 //     console.log("Hi")
 // }
-
-
 // const add = (a:number ,b:number ,c:number | string =80)=>{
 //     console.log(a+b);
 //     console.log(c)
-    
-
 // }
-
 // add(4,7)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // any types
 // let age : any =34;
 // age=true
 // console.log(age);
 // age='Joseph';
 // console.log(age);
-
 // let mixed : any[]=[];
 // mixed.push(5)
 // mixed.push('Joseph')
-
 // console.log(mixed)
-
 // let ninja :{name:any ,age:any};
-
 // ninja={name:'Joseph', age:25}
 // console.log(ninja)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // explicit types
 // let character: string;
 // let age:number;
 // let isLoggedIn: boolean;
-
 // //arrays
 // let ninjas: string[]=[];
-
 // ninjas.push('joe')
-
 // let mixed :(string | number | boolean )[]=[];
-
 // mixed.push('hello')
 // mixed.push(20)
 // mixed.push(true)
-
 // console.log(mixed)
-
 // //union types
-
 // let uid : string|number
 // uid='123'
 // uid=123
-
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,150 @@
-"use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a ui of ${uid}`);
-};
-const greet = (user) => {
-    console.log(`${user.name} says hi`);
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //TYPE ALIASES
+// type StringorNum = string | number;
+// type objWithName = {name:string ,uid:StringorNum}
+
+
+// const logDetails = (uid: StringorNum , item: string)=>{
+//     console.log(`${item} has a ui of ${uid}`)
+// }
+
+// const greet =(user:objWithName) =>{
+//     console.log(`${user.name} says hi`);
+    
+// }
+
+
+
+
+
 //FUNCTIONS
 // let greet : Function
+
 // greet= ()=>{
 //     console.log("Hi")
 // }
+
+
 // const add = (a:number ,b:number ,c:number | string =80)=>{
 //     console.log(a+b);
 //     console.log(c)
+    
+
 // }
+
 // add(4,7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // any types
 // let age : any =34;
 // age=true
 // console.log(age);
 // age='Joseph';
 // console.log(age);
+
 // let mixed : any[]=[];
 // mixed.push(5)
 // mixed.push('Joseph')
+
 // console.log(mixed)
+
 // let ninja :{name:any ,age:any};
+
 // ninja={name:'Joseph', age:25}
 // console.log(ninja)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // explicit types
 // let character: string;
 // let age:number;
 // let isLoggedIn: boolean;
+
 // //arrays
 // let ninjas: string[]=[];
+
 // ninjas.push('joe')
+
 // let mixed :(string | number | boolean )[]=[];
+
 // mixed.push('hello')
 // mixed.push(20)
 // mixed.push(true)
+
 // console.log(mixed)
+
 // //union types
+
 // let uid : string|number
 // uid='123'
 // uid=123
+
