8030a1b5af63145b3d6c478b7087cb960a4fba73
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-01 10:12:18 +0100

    init

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
 "use strict";
 //Working with DOM
-const anchor = document.querySelector('a');
-console.log(anchor);
+const form = document.querySelector('.new-item-form');
+// console.log(form.children)
+//inputs
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
 // //TYPE ALIASES
 // type StringorNum = string | number;
 // type objWithName = {name:string ,uid:StringorNum}
